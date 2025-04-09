4dc3c1e9bfa26df1d604216fff46410afcbaa642
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-04-09 17:26:26 +0300

    aDD
    quit
    
    ''
    q
    
    clear

---
+++ NYT-News/Views/BreakingNewsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  BreakingNewsCollectionViewCell.swift
+//  NYT-News
+//
+//  Created by Beyza Nur Tekerek on 9.04.2025.
+//
+
+import UIKit
+
+class BreakingNewsCollectionViewCell: UICollectionViewCell {
+    
+}
