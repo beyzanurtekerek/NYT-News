2a832b637c05226e4868a5f7917d0b9ac8d3094d
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-04-09 16:56:40 +0300

    Implemented MainTabBarVC controller

--- NYT-News/Controllers/Core/MainTabBarVC.swift
+++ NYT-News/Controllers/Core/MainTabBarVC.swift
@@ -11,7 +11,22 @@ class MainTabBarVC: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let vc1 = UINavigationController(rootViewController: HomeVC())
+        let vc2 = UINavigationController(rootViewController: SearchVC())
+        let vc3 = UINavigationController(rootViewController: SavedVC())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc3.tabBarItem.image = UIImage(systemName: "bookmark")
+        
+        vc1.title = "Home"
+        vc2.title = "Search"
+        vc3.title = "Saved"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1, vc2, vc3], animated: true)
         
     }
 
