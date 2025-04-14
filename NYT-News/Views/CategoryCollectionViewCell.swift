14cfebf706428737a09ec0d38c371990fa159b26
beyzanurtekerek <beyzanrtkrk@gmail.com>
2025-04-14 23:34:58 +0300

    Add search vc cells

---
+++ NYT-News/Views/CategoryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoryCollectionViewCell.swift
+//  NYT-News
+//
+//  Created by Beyza Nur Tekerek on 14.04.2025.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "CategoryCollectionViewCell"
+    
+}
---
+++ NYT-News/Views/DiscoverCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SearchCollectionViewCell.swift
+//  NYT-News
+//
+//  Created by Beyza Nur Tekerek on 14.04.2025.
+//
+
+import UIKit
+
+class DiscoverCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "DiscoverCollectionViewCell"
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: <#T##String#>)
+    }
+    
+}
